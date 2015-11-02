57bd34022e8972bb175e1762f71c45618ffddf43
Marcel <Marcel@Marcel-TOSH>
2015-11-02 13:23:42 +0100

    wg  gwergwe rgeg erg

---
+++ AnimationGame/src/PaintableArrayPanel.java
@@ -0,0 +1,12 @@
+
+public class PaintableArrayPanel extends SizedPanel{
+	
+	Paintable[] ps;
+	
+	public PaintableArrayPanel(Paintable[] pa){ps=pa;}
+	
+	public void paintComponent(java.awt.Graphics g){
+		for(Paintable p:ps) p.paintTo(g);
+	}
+
+}
